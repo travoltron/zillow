c47528e43f05741ac2abee0a89112391aee89919
travoltron <ben@travoltron.com>
2016-07-26 15:14:28 -0400

    Taking out Goutte

--- src/yajra/Zillow/ZillowClient.php
+++ src/yajra/Zillow/ZillowClient.php
@@ -1,7 +1,6 @@
 <?php namespace yajra\Zillow;
 
 use GuzzleHttp\Client as GuzzleClient;
-use Goutte\Client as GoutteClient;
 use GuzzleHttp\ClientInterface as GuzzleClientInterface;
 use yajra\Zillow\ZillowException;
 
@@ -188,53 +187,6 @@ class ZillowClient
         if (in_array($name, self::$validCallbacks)) {
             return $this->doRequest($name, $arguments);
         }
-    }
-
-    /**
-     * Since zillow does not provide the ability to grab the photos
-     * of the properties through the API this little method will scan
-     * the property url and grab all the images for that property
-     * @param string $uri
-     * @return array
-     */
-    public function getPhotos($uri)
-    {
-        $this->photos = [];
-        $client       = new GoutteClient;
-        $crawler      = $client->request('GET', $uri);
-
-        // Get the latest post in this category and display the titles
-        $crawler->filter('.photos a')->each(function ($node) {
-            $this->photos[] = $node->filter('img')->attr('src') ?: $node->filter('img')->attr('href');
-        });
-
-        $this->response = $this->photos;
-
-        return $this->response;
-    }
-
-    /**
-     * see @GetPhotos
-     * Works the same way but instead passing a uri
-     * you can pass a zpid and it will perform a request to grab the uri
-     * based on the id
-     * @see GetPhotos
-     * @param int @zpid
-     * @return array
-     */
-    public function getPhotosById($zpid)
-    {
-        // We call the GetZestimate first to get the link to the home page details
-        $response = $this->GetZestimate(['zpid' => $zpid]);
-
-        $this->photos = [];
-        if ($this->isSuccessful() && isset($response['links']['homedetails']) && $response['links']['homedetails']) {
-            return $this->GetPhotos($response['links']['homedetails']);
-        } else {
-            $this->setStatus(999, 'COULD NOT GET PHOTOS');
-        }
-
-        return $this->response;
     }
 
     /**
